4570435bb795fdfe58e46b3fb6b1b3eb4bde11eb
aaye420 <ishant.ishant.1@gmail.com>
2022-11-20 04:10:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.use(express.static(__dirname + '/dist/<app-name>'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/<interface>/index.html'));});
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
+
+var port = process.env.PORT || 3000;
+
+app.listen(port);
