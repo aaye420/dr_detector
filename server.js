dc391343d859996f7d7f521342965a688c0b3011
aaye420 <ishant.ishant.1@gmail.com>
2022-11-20 04:47:32 +0530

    m

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/dr_detector'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<interface>/index.html'));});
+'/dist/dr_detector/index.html'));});
 app.listen(process.env.PORT || 8080);
